9b8c8c64b6f3e3780ff2f3a94407cf0f113d202d
Estebanfonseca <estebanp57@hotmail.com>
2022-11-10 14:14:31 -0500

    model hotels y creacion

--- models/Hotel.js
+++ models/Hotel.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 
 let schema = new mongoose.Schema({
     name:{type:String,required:true},
-    photo:{type:String,required:true},
+    photo:[{type:String,required:true}],
     capacity:{type:Number,required:true},
-    cityID:{}
+    cityID:{type: mongoose.Types.ObjectId, ref:'cities',required: true},
+    userID:{type: mongoose.Types.ObjectId, ref: 'users',required: true}
 })
+
+
+const Hotel = mongoose.model('hotels',schema) 
+module.exports = Hotel
