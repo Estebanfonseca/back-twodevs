0d55cfa0a8a6dd1bc7dd43eee3b3c30c6e149f6b
Emilio Lubo <emilubo@msn.com>
2022-11-10 17:43:37 -0300

    Route create citie created

--- controllers/users.js
+++ controllers/users.js
@@ -2,13 +2,22 @@ const User = require('../models/User')
 
 const controller = {
     create: async(req, res) => {
+        let newEmail = req.body.email.toLowerCase()
         try{
-            let newUser = await User.create(req.body)
-            res.status(201).json({
-                id: newUser._id,
-                success: true,
-                message: "new user created"
-            })
+            let alreadyExist = await User.find({email: { $regex : new RegExp(`^${newEmail}$`, 'i') }})
+            if(alreadyExist.length > 0){
+                res.status(400).json({
+                    success: false,
+                    message: "user email already exists"
+                })
+            } else{
+                let newUser = await User.create(req.body)
+                res.status(201).json({
+                    id: newUser._id,
+                    success: true,
+                    message: "new user created"
+                })
+            }
         } catch(err){
             res.status(400).json({
                 success: false,
