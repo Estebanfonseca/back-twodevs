9874b479e3088a3141f7fe95781de32927f38069
Estebanfonseca <estebanp57@hotmail.com>
2022-11-11 11:06:46 -0500

    hotels get

--- controllers/hotels.js
+++ controllers/hotels.js
@@ -24,6 +24,21 @@ const controller = {
                 message: err.message
             })
         }
+    },
+    read: async (req,res)=>{
+        try{
+            let hotel = await Hotel.find()
+            res.status(200).json({
+                response: hotel,
+                success:true,
+                message: 'all hotels are here!'
+            })
+        }catch(err){
+            res.status(400).json({
+                success: false,
+                message: err.message
+            })
+        }
     }
 } 
 
