a37546ecd5782d4ab437cf766f739d286f3a829f
Emilio Lubo <emilubo@msn.com>
2022-11-17 12:04:56 -0300

    update branch

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.use('/api', (req, res) => {
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404))
-});
-
+})
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -40,7 +39,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
-});
+  res.render('error')
+})
 
 module.exports = app;
