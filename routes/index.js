9ab339b414984e52d4d329b8297b8f0751df7f7f
Estebanfonseca <estebanp57@hotmail.com>
2022-12-01 19:06:44 -0500

    controller y routes

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,14 @@ let citiesRouter = require('./cities')
 let hotelsRouter = require('./hotels')
 let showsRouter = require('./shows')
 let itinerariesRouter = require('./itineraries')
+let comments = require('./comments')
 
 router.use('/auth', usersRouter)
 router.use('/cities', citiesRouter)
 router.use('/hotels', hotelsRouter)
 router.use('/shows' ,showsRouter)
 router.use('/itineraries', itinerariesRouter)
+router.use('/comments', comments)
 
 
 module.exports = router;
--- routes/shows.js
+++ routes/shows.js
@@ -2,11 +2,13 @@ let router = require('express').Router()
 let passport = require('../config/passport')
 let {create, update, destroy,read} = require('../controllers/shows')
 const showUserIdExists = require('../middlewares/showUserIdExists')
+let validator = require('../middlewares/validator')
+let schema = require('../schemas/show')
 
 
 router.post('/', passport.authenticate('jwt',{session:false}), create)
 router.get('/', read)
-router.patch('/:id', passport.authenticate('jwt',{session:false}), showUserIdExists, update)
+router.patch('/:id',validator(schema), passport.authenticate('jwt',{session:false}), showUserIdExists, update)
 router.delete('/:id', passport.authenticate('jwt',{session:false}), showUserIdExists, destroy)
 
 
