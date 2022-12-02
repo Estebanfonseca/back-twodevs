1a1880447e9b7b9c1a774d2571688b59f9673002
Emilio Lubo <emilubo@msn.com>
2022-12-02 00:03:26 -0300

    fix

--- middlewares/userIdAuthorized.js
+++ middlewares/userIdAuthorized.js
@@ -3,7 +3,7 @@ const Reaction = require("../models/Reaction")
 
 const userIdAuthorized = async(req,res,next) => {
         let reaction = await Reaction.find({_id: req.params.id, userId: {$in: [req.user._id]}})
-        if(reaction.length > 0){
+        if(reaction){
             return next()
         } else{
             return idDontMatchResponse(req, res)
